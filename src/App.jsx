cc8364a712ae39927fea9762bf3761d232fb0e38
muhammadasif-wd <muhammadasif.wd@gmail.com>
2023-12-13 23:47:16 +0600

    [DONE] add next ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-slate-700 font-bold text-center py-5 text-dark 3xl:text-8xl 2xl:text-6xl xl:text-4xl lg:text-2xl md:text-xl sm:text-lg text-sm">
+    <div className="flex justify-center items-center text-dark gap-5">
+      <h1 className="text-slate-700 font-bold text-center py-5 3xl:text-8xl 2xl:text-6xl xl:text-4xl lg:text-2xl md:text-xl sm:text-lg text-sm">
         Hello World
-        <span class="material-icons">settings</span>
       </h1>
+      <span class="material-icons">settings</span>
     </div>
   );
 }
