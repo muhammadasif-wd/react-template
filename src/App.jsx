b41dbfab4c29406fd999710b0fa9892f5fcef2a3
muhammadasif-wd <muhammadasif.wd@gmail.com>
2023-12-14 00:02:43 +0600

    [DONE] setup eslint prettier husky

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex justify-center items-center text-dark gap-5">
       <h1 className="text-slate-700 font-bold text-center py-5 3xl:text-8xl 2xl:text-6xl xl:text-4xl lg:text-2xl md:text-xl sm:text-lg text-sm">
-        Hello World
+        Hello World This Home Page
       </h1>
       <span className="material-icons">settings</span>
     </div>
